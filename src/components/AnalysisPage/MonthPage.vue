d382f8d7b29a934302032891d632f38547a8fb28
rjswn1031 <78711965+rjswn1031@users.noreply.github.com>
2021-04-30 16:49:18 +0900

    월간 수입 지출 그래프 완료
    월간 수입 지출 그래프 완료
    월간 카테고리 그래프 초안 생성

--- src/components/AnalysisPage/MonthPage.vue
+++ src/components/AnalysisPage/MonthPage.vue
@@ -1,22 +1,41 @@
 <template>
   <div>
-      <MoneyAnalysis :nowDate="nowMonth"/>
+      <section>
+          <MoneyAnalysis :nowDate="nowMonth"/>
+      </section>
+      <section>
+          <ChartAnalysis :nowDate="nowMonth"/>
+      </section>
+      <section>
+          <CategoryChart :nowDate="nowMonth"/>
+      </section>
   </div>
 </template>
 
 <script>
 import MoneyAnalysis from './MoneyAnalysis/MoneyAnalysis.vue'
+import ChartAnalysis from './ChartAnalysis/ChartAnalysis.vue'
+import CategoryChart from './ChartAnalysis/CategoryChart.vue'
+
 import Common from '../../assets/common.js'
 var common = new Common;
 
 export default {
     components: {
-        MoneyAnalysis
+        MoneyAnalysis,
+        ChartAnalysis,
+        CategoryChart
     },
     data() {
         return {
             nowMonth: common.fget_DateFormat(new Date(), 'month')
         }
+    },
+    created() {
+        
+    },
+    methods: {
+        
     }
 }
 </script>
