df025ff5ee639160bbf49890f82cff8073330f9d
rjswn1031 <78711965+rjswn1031@users.noreply.github.com>
2021-05-12 14:41:56 +0900

    login페이지 CSS
    login페이지 CSS

--- frontend/src/components/LoginPage.vue
+++ frontend/src/components/LoginPage.vue
@@ -1,20 +1,22 @@
 <template>
   <div id="loginPage">
     <img id="logo" src="../assets/coin.png" alt="">
+    <div id="logoText" class="mx-auto">coWin</div>
     <table class="mx-auto" id="loginContainer">
       <tr>
-        <td>ID</td>
-        <td><b-form-input id="input-id" v-model="id" placeholder="Enter ID" required></b-form-input></td>
+        <td colspan="2"><b-form-input class="loginInput" id="input-id" v-model="id" placeholder="Enter ID" required></b-form-input></td>
       </tr>
       <tr>
-        <td>PassWord</td>
-        <td><b-form-input id="input-pass" type="password" v-model="pass" placeholder="Enter Password" required></b-form-input></td>
+        <td colspan="2"><b-form-input class="loginInput" id="input-pass" type="password" v-model="pass" placeholder="Enter Password" required></b-form-input></td>
       </tr>
       <tr>
-        <button @click="fget_loginChk">로그인</button>
+        <td colspan="2">
+          <button id="loginBtn" @click="fget_loginChk">로그인</button>
+        </td>
       </tr>
       <tr>
-        <button>회원가입</button>
+        <td><button id="newMember" class="memberBtn">회원 가입</button></td>
+        <td><button id="findMember" class="memberBtn">회원 찾기</button></td>
       </tr>
       <router-link to="main">임시</router-link>
     </table>
@@ -47,7 +49,17 @@ export default {
 </script>
 
 <style>
+.loginInput {height: 50px !important; color: #4fc08d !important; }
+
 #loginPage {background-color: #4fc08d; width: 100vw; height: 100vh; box-sizing: border-box; text-align: center;}
-#loginContainer {position: absolute; background-color: #f2f2f2; width: 30vw; height: 33vh; left: 35vw; top: 430px; }
-#logo {margin-top: 30px;background-color: #4fc08d; width: 400px; height: 400px;}
+#loginContainer {position: absolute; width: 30vw; height: 33vh; left: 35vw; top: 470px; border-radius: 5px;}
+
+#logo {margin-top: 0px; background-color: #4fc08d; width: 500px; height: 500px; opacity: 0.5;}
+#logoText {position: absolute; top: 200px; width: 100vw; color: white; font-weight: bold; font-size: 100px; text-align: center;}
+#loginBtn {width: 100%; height: 50px; background-color: white; color: #4fc08d; font-weight: bold; border-radius: 5px; border: 1px solid white; margin-top: 5px;}
+#loginBtn:hover {background-color: #4fc08d; color: white;}
+
+.memberBtn {background: #4fc08d; border: 1px #4fc08d; color: white; margin-top: 10px;}
+.memberBtn:hover {color: #35495E;}
+
 </style>
