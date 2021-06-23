4f147ac78b377b50ae9ac63cdc9c426a5d45f4dd
rjswn1031 <78711965+rjswn1031@users.noreply.github.com>
2021-06-23 18:02:02 +0900

    memopopup 추가
    memopopup 추가

--- frontend/src/components/MainPage.vue
+++ frontend/src/components/MainPage.vue
@@ -3,27 +3,36 @@
     <MenuBar/>
     <router-view/>
     <div class="memoIconContainer">
-      <div class="memoIconBtn">
+      <div class="memoIconBtn" v-if="isMemoBtnOn" @click="openMomoPopup" >
         <b-icon class="memoIcon" icon="journal-text"></b-icon>
       </div>
     </div>
+    <MemoPopup v-if="!isMemoBtnOn" :closeMemoPopup="closeMemoPopup" />
   </div>
 </template>
 
 <script>
-import MenuBar from '../components/MenuBar/MenuBar.vue'
+import MenuBar from './MenuBar/MenuBar.vue'
+import MemoPopup from './Memo/MemoPopup.vue'
 
 export default {
   name: 'App',
   components: {
-    MenuBar
+    MenuBar,
+    MemoPopup
   },
   data() {
     return {
+      isMemoBtnOn: true
     }
   },
   methods: {
-
+    openMomoPopup() {
+      this.isMemoBtnOn = false;
+    },
+    closeMemoPopup(){
+      this.isMemoBtnOn = true;
+    }
   }
 }
 </script>
@@ -32,5 +41,5 @@ export default {
 section { border: 0em !important; }
 #MainPage { display: flex; flex-direction: column; align-content: center; width: 100%; height: 100%; }
 #MainPage > .memoIconContainer { display: flex; justify-content: center; position: absolute; bottom: 0em; width: 100%; }
-#MainPage > .memoIconContainer > .memoIconBtn { display: flex; justify-content: center; align-items: center; width: 5em; height: 2em; font-size: 1.5em; border: 1px solid gray; background-color: white; z-index: 100; border-top-left-radius: .7em; border-top-right-radius: .7em; }
+#MainPage > .memoIconContainer > .memoIconBtn { display: flex; justify-content: center; align-items: center; width: 5em; height: 1.7em; font-size: 1.5em; transition: all 0.3s; border: 1px solid gray; background-color: white; z-index: 100; border-top-left-radius: .7em; border-top-right-radius: .7em; }
 </style>
